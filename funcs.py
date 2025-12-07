d2e0694c6b40f287c26f9ed89e9f634cbdae91aa
Bobakj <karolartur2334@gmail.com>
2025-12-07 14:10:34 +0300

    Add files via upload

---
+++ funcs.py
@@ -0,0 +1,80 @@
+import data
+
+quest_index = 0
+quests_data = data.questions
+
+#Обработка исключений
+def generate_quest(QUEST, buttons):
+    if quest_index < len(quests_data):
+        quest = quests_data[quest_index]["quest"]
+        QUEST.config(text=quest)
+        for i in range(len(buttons)):
+            buttons[i].config(text=quests_data[quest_index]["answers"][i])
+    else:
+        QUEST.config(text="Викторина закончена!")
+        #Блокировка кнопок
+        for btn in buttons:
+            btn.config(state="disabled")
+
+#Функция проверки ответа и генерации нового вопроса
+def choice(button, QUEST, buttons, info):
+    global quest_index #контролирует порядковый номер вопроса
+    if button["text"] == quests_data[quest_index]["right"]:
+        quest_index += 1
+        info.config(text="")
+        generate_quest(QUEST, buttons)
+    else:
+        info.config(text="Ответ неверный")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
